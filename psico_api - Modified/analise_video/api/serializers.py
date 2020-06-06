2e0322833979454dfed3d83da327442c82494a6d
Matheus Faino <matheusmrqfaino@gmail.com>
2020-06-05 22:41:46 -0400

    Salvando novos testes
    Adicionando a funcionalidade de analisar o video

--- psico_api - Modified/analise_video/api/serializers.py
+++ psico_api - Modified/analise_video/api/serializers.py
@@ -1,9 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from analise_video.models import AnaliseVideo
 
 
-class AnaliseVideoSerializer(ModelSerializer):
+class AnaliseVideoSerializer(serializers.ModelSerializer):
+    video = serializers.CharField(read_only=True)
+
     class Meta:
         model = AnaliseVideo
-        fields = ('possivel_depressao', 'porcentagem_emocao1', 'porcentagem_emocao2',
-                  'porcentagem_emocao3', 'data')
+        fields = ('id', 'possivel_depressao', 'porcentagem_emocao1', 'porcentagem_emocao2',
+                  'porcentagem_emocao3', 'data', 'video')
+        read_only_fields = ['__all__']
--- psico_api - Modified/reconhece/testando.py
+++ psico_api - Modified/reconhece/testando.py
@@ -1,6 +1,11 @@
 import reconhece.reconhecer as reconhecer
 
-variavel = reconhecer.Reconhecer("videos/meu_video.mp4")
+caminho = '../media_images/videos/'
+video = 'http://127.0.0.1:8000/media_files/videos/meu_video.mp4'
+video = video.split('videos')
+
+
+variavel = reconhecer.Reconhecer(caminho + video[1])
 variavel2 = variavel.realizar_reconhecimento()
 print(variavel2)
 print("AAAAa")
